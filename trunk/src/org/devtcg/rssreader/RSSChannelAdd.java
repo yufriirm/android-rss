f17e574b25cd03e5e4641320a92b68e081c423b5
jasta <jasta@29467b05-4340-0410-ab72-675ca585a781>
2007-11-23 06:45:05 +0000

    Oops

--- trunk/src/org/devtcg/rssreader/RSSChannelAdd.java
+++ trunk/src/org/devtcg/rssreader/RSSChannelAdd.java
@@ -7,6 +7,7 @@ package org.devtcg.rssreader;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
